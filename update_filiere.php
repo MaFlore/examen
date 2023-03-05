112c4fb181cddf2110cf2fa5440d4d73cc189252
AHADJITSE Yawo Florent Mathis <yawo.ahadjitse@ipnetinstitute.com>
2023-03-05 11:37:36 +0000

    Ajout du boutton de deconnexion dans le fichier update_filiere.php

--- update_filiere.php
+++ update_filiere.php
@@ -25,6 +25,12 @@ $reponse = $filiere->fetch(PDO::FETCH_OBJ);
         <?php include('navigation.php') ?>
     </nav>
     <div class="container">
+        <div style="text-align: right;">
+            <h5>Vous êtes connecté M/Mlle <?php echo $_SESSION['nom'].' '.$_SESSION['prenom']; ?></h5>
+            <a href="">
+                <button type="button" class="btn rounded-pill btn-info">Deconnectez-vous !</button>
+            </a>
+        </div>
     <h2>Modification d'une filière</h2>
     <a href="examen/filieres.php"><button type="button" class="btn rounded-pill btn-success">Retour</button></a>
     <form class="form-horizontal" method="post" action="examen/update_filiere_db.php">
