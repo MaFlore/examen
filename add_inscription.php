b7bc468098eaffa66c75933c88016ae151787683
AHADJITSE Yawo Florent Mathis <yawo.ahadjitse@ipnetinstitute.com>
2023-03-05 11:34:19 +0000

    Ajout du boutton de deconnexion dans le fichier add_inscription.php

--- add_inscription.php
+++ add_inscription.php
@@ -30,6 +30,12 @@
         <?php include('navigation.php') ?>
     </nav>
     <div class="container">
+        <div style="text-align: right;">
+            <h5>Vous êtes connecté M/Mlle <?php echo $_SESSION['nom'].' '.$_SESSION['prenom']; ?></h5>
+            <a href="">
+                <button type="button" class="btn rounded-pill btn-info">Deconnectez-vous !</button>
+            </a>
+        </div>
     <h2>Ajout d'une inscription</h2>
     <a href="examen/inscriptions.php"><button type="button" class="btn rounded-pill btn-success">Retour</button></a>
     <form class="form-horizontal" method="post" action="examen/add_inscription_db.php">
